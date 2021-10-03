85ba29e69fa2cf6cec3563ac108beea83c5019a9
astronmax <astronmax@yandex.ru>
2021-10-03 19:03:11 +0300

    Add lab 1

---
+++ src/labs/lab_1/Main.java
@@ -0,0 +1,10 @@
+package labs.lab_1;
+
+public class Main
+{
+    public static void main(String[] args) {
+        for (double n = 1.0; n <= 10.0; ++n) {
+            System.out.println(1/n + " = 1/" + n);
+        }
+    }
+}
